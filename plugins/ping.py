55e1b4b2860d6748987495b14d145378f06b6a05
ZaenProjec <91957490+Zaen-ubot@users.noreply.github.com>
2022-01-13 17:21:34 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -18,7 +18,7 @@ Disk: {disk}%'''
     return stats
 
 
-@app.on_message(filters.command(["ping", "ping@TheLittleMusicPrivateBot"]))
+@app.on_message(filters.command(["ping", "pingTheLittleMusicPrivateBot"]))
 async def ping(_, message):
     uptime = await bot_sys_stats()
     start = datetime.now()
