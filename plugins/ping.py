24a2d34799a4196100a14610218db816c38639c8
Xyren <96537396+Xyren-64bit@users.noreply.github.com>
2022-01-13 22:00:59 +0800

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -2,31 +2,31 @@
 from pyrogram import filters, Client
 from pyrogram.types import Message
 import os
+import shutil
 import psutil
+from sys import version
 import time
+from utils.sys_stats import humanbytes
 from datetime import datetime
-
-async def bot_sys_stats():
-    bot_uptime = int(time.time() -vdcall _START_TIME)
-    cpu = psutil.cpu_percent(interval=0.5)
-    mem = psutil.virtual_memory().percent
-    disk = psutil.disk_usage("/").percent
-    stats = f'''
-Uptime: {get_readable_time((bot_uptime))}
-CPU: {cpu}%
-RAM: {mem}%
-Disk: {disk}%'''
-    return stats
 
 
 @Client.on_message(filters.command('ping') & ~filters.private & ~filters.channel)
 async def ping(_, message):
-    uptime = await bot_sys_stats()
     start = datetime.now()
-    response = await message.reply_photo(
-        photo="cache/Query.png",
-        caption=">> Pong!"
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = f"{psutil.cpu_percent()} %"
+    ram_usage = f"{psutil.virtual_memory().percent} %"
+    response = await message.reply_text(
+        ">> Pong!"
     )
     end = datetime.now()
     resp = (end - start).microseconds / 1000
-    await response.edit_text(f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>Music System Stats:</u></b>{uptime}")
+    await response.edit_text(f"""**Pong!**`⚡{resp} ms`
+     
+<b><u>Music System Stats:</u></b>
+RAM:{ram_usage}
+CPU:{cpu_usage}
+Used:{used}({disk_usage}""")
